d9c89a7a21192214d1b93e58af133372dce1d127
qi0523 <1426042225@qq.com>
2022-09-16 17:27:42 +0800

    add nerdctl

--- containerd_k8s_image.sh
+++ containerd_k8s_image.sh
@@ -40,6 +40,7 @@ wget https://github.com/containerd/containerd/releases/download/v1.5.11/containe
 wget https://github.com/opencontainers/runc/releases/download/v1.1.4/runc.amd64
 wget https://github.com/containernetworking/plugins/releases/download/v1.1.1/cni-plugins-linux-amd64-v1.1.1.tgz
 wget https://raw.githubusercontent.com/containerd/containerd/main/containerd.service
+wget https://github.com/containerd/nerdctl/releases/download/v0.23.0/nerdctl-0.23.0-linux-amd64.tar.gz
 
 sudo tar Cxzvf /usr/local containerd-1.5.11-linux-amd64.tar.gz
 sudo mkdir -p /usr/local/lib/systemd/system
@@ -52,6 +53,10 @@ sudo install -m 755 runc.amd64 /usr/local/sbin/runc
 
 sudo mkdir -p /opt/cni/bin
 sudo tar Cxzvf /opt/cni/bin cni-plugins-linux-amd64-v1.1.1.tgz
+
+#nerdctl
+sudo tar -xvzf nerdctl-0.23.0-linux-amd64.tar.gz -C /usr/bin nerdctl
+sudo mkdir -p /var/lib/nerdctl
 
 ############################ containerd config.toml
 sudo mkdir -p /etc/containerd/
